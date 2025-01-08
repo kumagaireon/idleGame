4743589801197d1051afba223cd4ef1584c97652
豊 豊枝 <yutaka150410@gmail.com>
2025-01-08 12:21:12 +0900

    コール用のオブジェクト表示完了

--- Assets/[yutaka2]/Scripts 1/Note/NoteTap.cs
+++ Assets/[yutaka2]/Scripts 1/Note/NoteTap.cs
@@ -7,20 +7,33 @@ public class NoteTap : MonoBehaviour
     private List<int> tappedCount = new List<int>();
 
     [Header("タップ可能オブジェクト")]
-    [SerializeField] private GameObject tapAbleObject;
+    [SerializeField] private GameObject[] tapAbleObject;
+
+    [Header("上昇速度")]
+    [SerializeField] private float moveSpeed = 0.3f;
+
+    [Header("初期y座標")]
+    [SerializeField] private float startPosY = -10.0f;
+
+    [Header("生存時間")]
+    [SerializeField] private float lifeTime = 2.0f;
 
     //タップ可能時間
     public async Task OnTapAble()
     {
+        int index = 0;
+
         //入力の受付開始
         //InputChecker.instance.SetTapAble();
-        tapAbleObject.SetActive(true);
+        ChangeToStartPosition(index);
+        tapAbleObject[index].SetActive(true);        
 
         //=====入力の回数カウント=====
         int counter = 0;
         float timer = 0;
-        while (timer < 1.0f)
+        while (timer < lifeTime)
         {
+            MoveObject(index);
             timer += Time.deltaTime;
             if (InputChecker.instance.TappedEnter())
             {
@@ -34,7 +47,24 @@ public class NoteTap : MonoBehaviour
         //入力の受付終了
         Debug.Log("tap終了 count:" + counter);
         tappedCount.Add(counter);
-        tapAbleObject.SetActive(false);
+        tapAbleObject[index].SetActive(false);
         //InputChecker.instance.SetTapNotAble();
+    }
+
+    private int DecideRandom()
+    {
+        int random = Random.Range(0, tapAbleObject.Length);
+        return random;
+    }
+
+    private void ChangeToStartPosition(int index)
+    {
+        float posX = tapAbleObject[index].transform.position.x;
+        tapAbleObject[index].transform.position = new Vector2(posX, startPosY);
+    }
+
+    private void MoveObject(int index)
+    {
+        tapAbleObject[index].transform.Translate(Vector2.up * moveSpeed);
     }
 }
