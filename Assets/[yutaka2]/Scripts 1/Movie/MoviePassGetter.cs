df8566d52593aad588af9c2d0dbbef2b071d53d2
豊 豊枝 <yutaka150410@gmail.com>
2025-01-09 15:41:54 +0900

    動画パスを受け取るクラスを追加 StreamingAsstesフォルダの中身を変更したので注意お願いします

---
+++ Assets/[yutaka2]/Scripts 1/Movie/MoviePassGetter.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.Video; // Video Playerを使用するために必要
+
+public class MoviePassGetter : MonoBehaviour
+{
+    [SerializeField] private VideoPlayer videoPlayer; // Video Playerコンポーネントの参照
+    private string videoFileName; // 動画ファイル名
+
+    public string VideoPath
+    {
+        get => VideoPath;
+        set => VideoPath = value;
+    }    
+
+    void Start()
+    {
+        if (videoPlayer == null)
+        {
+            // Video Playerコンポーネントが設定されていない場合は、自動的に取得
+            videoPlayer = GetComponent<VideoPlayer>();
+            videoFileName = "Ryo_LOVEGUN.mp4";
+        }
+
+        PlayVideoByFileName(videoFileName);
+    }
+
+    /// <summary>
+    /// ファイル名を指定して動画を再生
+    /// </summary>
+    /// <param name="fileName">再生する動画のファイル名（拡張子含む）</param>
+    public void PlayVideoByFileName(string fileName)
+    {
+        if (string.IsNullOrEmpty(fileName))
+        {
+            Debug.LogError("ファイル名が指定されていません");
+            return;
+        }
+
+        // StreamingAssetsフォルダ内の動画パスを設定
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, fileName);
+        
+        // 動画ファイルの存在確認
+        if (!System.IO.File.Exists(videoPath))
+        {
+            Debug.LogError($"動画ファイルが見つかりません: {videoPath}");
+            return;
+        }
+
+        // 動画のURLを設定して再生
+        videoPlayer.url = videoPath;
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
